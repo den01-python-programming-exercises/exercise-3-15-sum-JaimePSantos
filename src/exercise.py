96869dedcc1cd772a99361cce4fa60a25f39aa37
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-22 11:29:15 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    print(sum_list(range(0,5)))
+def sum_list(numbers):
+  soma = 0
+  for number in numbers:
+    soma+=number
+  return soma
 
 if __name__ == '__main__':
     main()
